3f9243731fc4208e88121cbfcce48f145a440a7e
Marcin Wierzbicki <marcin.wierzbicki@appstery.com>
2015-06-16 00:35:35 +0200

    name refactoring

--- gulp/build.js
+++ gulp/build.js
@@ -18,7 +18,7 @@ module.exports = function(options) {
         quotes: true
       }))
       .pipe($.angularTemplatecache('templateCacheHtml.js', {
-        module: 'ruchjowSpa',
+        module: 'ruchjowplSpa', 
         root: 'app'
       }))
       .pipe(gulp.dest(options.tmp + '/partials/'));
