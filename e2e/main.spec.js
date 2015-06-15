0a94dfe99e7fd5e3fabeeb33c3ce9aed7a9f4874
Marcin Wierzbicki <marcin.wierzbicki@appstery.com>
2015-06-16 00:19:24 +0200

    remove specs

--- e2e/main.spec.js
+++ e2e/main.spec.js
@@ -8,7 +8,7 @@ describe('The main view', function () {
     page = require('./main.po');
   });
 
-  it('should include jumbotron with correct data', function() {
+  /*it('should include jumbotron with correct data', function() {
     expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
     expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
     expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
@@ -16,6 +16,6 @@ describe('The main view', function () {
 
   it('list more than 5 awesome things', function () {
     expect(page.thumbnailEls.count()).toBeGreaterThan(5);
-  });
+  });*/
 
 });
